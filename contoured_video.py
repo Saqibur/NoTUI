6e21e0e01cd412f79923ef894b701b8a03d1b769
Saqibur <saqibur.rahman98@gmail.com>
2019-12-20 20:55:43 +0600

    Add labeled ocr frames

--- contoured_video.py
+++ contoured_video.py
@@ -1,5 +1,10 @@
 import cv2 as cv
 import numpy as np
+import pytesseract
+import imutils
+from pyimagesearch.shapedetector import ShapeDetector
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 def getContoursFromFrame(frame):
     # image = cv.imread("Images/023.jfif")
@@ -15,7 +20,6 @@ def getContoursFromFrame(frame):
     _, contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
     print(len(contours))
-
     for contour in contours:
         area = cv.contourArea(contour)
         print(area)
@@ -25,7 +29,9 @@ def getContoursFromFrame(frame):
             cv.rectangle(result, (x,y), (x+w,y+h), (0,255,0), 4)
             cv.putText(result, 'Note', (x+w+10,y+h), 0, 1, (0,255,0))
             crop_img = result[y:y+h, x:x+w]
-            cv.imshow("croped.png", crop_img)
+            print(pytesseract.image_to_string(crop_img))
+            cv.imshow("Cropped Images", crop_img)
+        cv.waitKey(500)
 
     # cv.drawContours(result, contours, -1, (0, 255, 0), 3)
 
@@ -40,6 +46,6 @@ while True:
     frame = getContoursFromFrame(frame)
     cv.imshow("Frame", frame)
 
-    key = cv.waitKey(60)
+    key = cv.waitKey(500)
     if key == 27:
         break
